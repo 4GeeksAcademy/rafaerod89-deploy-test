7ee7adf818fa10627b3fe6cc260cb5914addb298
rafaerod89 <rafaerod89@gmail.com>
2024-09-11 08:43:52 +0000

    Test data endpoint

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,11 +6,21 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
+from api.commands import insert_test_data
+
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+@api.route('/deploy/setup', methods=['GET'])
+def handle_setup():
+    insert_test_data()
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    }
+
+    return jsonify(response_body), 200
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
